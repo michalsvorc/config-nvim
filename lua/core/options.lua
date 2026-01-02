69d61f8ee7d1811efe2928a73e82b8b6aff20ee3
Michal Svorc <dev@michalsvorc.com>
2026-01-02 14:38:10 +0100

    feat(options): add minimalistic defaults

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -3,4 +3,67 @@ vim.g.maplocalleader = ","
 
 local opt = vim.opt
 
+-- UI
+opt.number = true
+opt.relativenumber = true
+opt.numberwidth = 3
+opt.cursorline = true
+opt.termguicolors = true
+opt.signcolumn = "yes"
+opt.scrolloff = 4
+opt.laststatus = 3
+
+-- Editing behavior
+opt.expandtab = true
+opt.shiftwidth = 2
+opt.tabstop = 2
+opt.softtabstop = 2
+
+-- Searching
+opt.ignorecase = true
+opt.smartcase = true
+opt.incsearch = true
+opt.hlsearch = false
+opt.inccommand = "nosplit"
+
+-- Files and buffers
+opt.undofile = true
+opt.swapfile = false
+opt.undolevels = 10000
+opt.confirm = true
+
+-- Performance
+opt.updatetime = 250
+opt.timeoutlen = 300
+
+-- Clipboard
 opt.clipboard = "unnamedplus"
+
+-- Splits
+opt.splitright = true
+opt.splitbelow = true
+opt.splitkeep = "screen"
+
+-- Completion
+opt.completeopt = { "menu", "menuone", "noselect" }
+
+-- Indentation and wrapping
+opt.wrap = true
+opt.breakindent = true
+opt.showbreak = "-> "
+
+-- Popup
+opt.pumblend = 10
+opt.pumheight = 10
+
+-- Sessions
+opt.sessionoptions = { "buffers", "curdir", "tabpages", "winsize", "help", "globals", "skiprtp", "folds" }
+
+-- Invisible characters
+opt.list = true
+opt.listchars = {
+  trail = ".",
+  nbsp = "_",
+  tab = ">-"
+}
+opt.conceallevel = 0
