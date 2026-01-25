67bc44b12646167af69e8f81bf9febbd6c9bd2a0
Michal Svorc <dev@michalsvorc.com>
2026-01-25 17:20:47 +0100

    feat(plugins): disable loading of standard plugins

--- init.lua
+++ init.lua
@@ -1,1 +1,2 @@
+require("disabled")
 require("options")
---
+++ lua/disabled.lua
@@ -0,0 +1,24 @@
+-- Disable loading of standard plugins
+-- https://neovim.io/doc/user/plugins.html#standard-plugin-list
+
+-- matchparen
+-- vim.g.loaded_matchparen = 1
+
+-- netrw
+-- vim.g.loaded_netrw = 1
+-- vim.g.loaded_netrwPlugin = 1
+-- vim.g.loaded_netrwSettings = 1
+-- vim.g.loaded_netrwFileHandlers = 1
+
+-- gzip / tar / zip
+vim.g.loaded_gzip = 1
+vim.g.loaded_tar = 1
+vim.g.loaded_tarPlugin = 1
+vim.g.loaded_zip = 1
+vim.g.loaded_zipPlugin = 1
+
+-- 2html
+vim.g.loaded_2html_plugin = 1
+
+-- tutor
+vim.g.loaded_tutor_mode_plugin = 1
