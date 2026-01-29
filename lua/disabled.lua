446f743fdf427583cdc59896237c8b822234ae1c
Michal Svorc <dev@michalsvorc.com>
2026-01-29 01:31:49 +0100

    feat(plugins): disable loading of netrw

--- lua/disabled.lua
+++ lua/disabled.lua
@@ -5,10 +5,10 @@
 -- vim.g.loaded_matchparen = 1
 
 -- netrw
--- vim.g.loaded_netrw = 1
--- vim.g.loaded_netrwPlugin = 1
--- vim.g.loaded_netrwSettings = 1
--- vim.g.loaded_netrwFileHandlers = 1
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_netrwSettings = 1
+vim.g.loaded_netrwFileHandlers = 1
 
 -- gzip / tar / zip
 vim.g.loaded_gzip = 1
